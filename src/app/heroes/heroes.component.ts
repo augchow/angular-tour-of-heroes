d552cb0f48a5fbd4b8106f002764b0a0d5c165ca
Augustine Chow <aug_chow00@hotmail.com>
2018-07-17 18:01:06 +0800

    Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -14,18 +14,23 @@ export class HeroesComponent implements OnInit {
      id: 1,
      name: 'Windstorm'
    };*/
+   selectedHero: Hero;
+   heroes: Hero[];
 
-  heroes = HEROES;
-
-  selectedHero: Hero;
-
-  constructor() { }
+   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes():void{
+    this.heroService
+      .getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
